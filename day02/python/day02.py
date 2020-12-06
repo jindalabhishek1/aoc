25eb152c84e549cf2154d0fef6d1f8e0270b0fa1
Abhishek Jindal <abhishekjindal.jindal74@gmail.com>
2020-12-06 09:55:10 +0000

    Day 2 Part2 solved

--- day02/python/day02.py
+++ day02/python/day02.py
@@ -9,8 +9,13 @@ for line in fhand:
     inp = inp.split()
     inp[0] = inp[0].split('-')
     inp[1] = inp[1][:1]
-    # lst.append(inp)
-    res = inp[2].count(inp[1])
-    if res >= int(inp[0][0]) and res <= int(inp[0][1]): count += 1
+    # res = inp[2].count(inp[1])
+    # if res >= int(inp[0][0]) and res <= int(inp[0][1]): count += 1
+    print(inp)
+    if inp[1] == inp[2][int(inp[0][0]) - 1] and inp[1] != inp[2][int(inp[0][1]) - 1]:
+        count += 1
+    elif inp[1] != inp[2][int(inp[0][0]) - 1] and inp[1] == inp[2][int(inp[0][1]) - 1]:
+        count += 1
+    print ("inp: ", inp, "count: ", count, "\n")
 
 print (count)
