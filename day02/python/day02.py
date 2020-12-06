5c72198d4c9cd46ec4a38e0c48255b9652ce0e62
Abhishek Jindal <abhishekjindal.jindal74@gmail.com>
2020-12-06 10:05:35 +0000

    Day 2 (Refined with option for custom input)

--- day02/python/day02.py
+++ day02/python/day02.py
@@ -1,11 +1,5 @@
-import re
-
 count = 0
-
-fhand = open('../input.txt')
-for line in fhand:
-    line = line.rstrip()
-    inp = line
+def check(inp):
     inp = inp.split()
     inp[0] = inp[0].split('-')
     inp[1] = inp[1][:1]
@@ -13,9 +7,25 @@ for line in fhand:
     # if res >= int(inp[0][0]) and res <= int(inp[0][1]): count += 1
     print(inp)
     if inp[1] == inp[2][int(inp[0][0]) - 1] and inp[1] != inp[2][int(inp[0][1]) - 1]:
-        count += 1
+        return True
     elif inp[1] != inp[2][int(inp[0][0]) - 1] and inp[1] == inp[2][int(inp[0][1]) - 1]:
-        count += 1
+        return True
+
+choice = input("Do you want to provide custom input(y or Y: ")
+if choice == 'y' or choice == 'Y':
+    while True:
+        inp = input("Input: ")
+        if len(inp) < 1: break
+        res = check(inp)
+        if res is True: count += 1
+    print ("inp: ", inp, "count: ", count, "\n")
+else:
+    fhand = open('../input.txt')
+    for line in fhand:
+        line = line.rstrip()
+        inp = line
+        res = check (inp)
+        if res is True: count += 1
     print ("inp: ", inp, "count: ", count, "\n")
 
 print (count)
