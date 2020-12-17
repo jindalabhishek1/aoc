2b69011ea57ec62c3d116f2cdd56416d8b6e4518
Abhishek Jindal <abhishekjindal.jindal74@gmail.com>
2020-12-17 16:55:56 +0530

    Day 06 Part 01 Solved

---
+++ day06/python/day06.py
@@ -0,0 +1,48 @@
+def inp():
+    fname = input("Enter input file name: ")
+    # if (len(fname)) < 1: fname = "../sample_input.txt"
+    try:
+        fhand = open(fname)
+    except:
+        fname = "../sample_input.txt"
+        print("Didn't entered a valid file, choose sample input file")
+        fhand = open(fname)
+    groupList = list()
+    group = None
+    for line in fhand:
+        line = line.rstrip()
+        if line == '':
+            groupList.append(group)
+            group = None
+            continue
+        if group is None:
+            group = line
+            continue
+        group = group + line
+    groupList.append(group)
+    return groupList
+
+def calc_num_of_yes(groupList):
+    totalList = list()
+    for group in groupList:
+        listOfQuestions = list()
+        for question in group:
+            count = listOfQuestions.count(question)
+            if count == 0:
+                listOfQuestions.append(question)
+        length = len(listOfQuestions)
+        totalList.append(length)
+    return totalList
+
+def calc_total_yes(listOfTotalYes):
+    sum = 0
+    for num in listOfTotalYes:
+        sum += num
+    return sum
+
+groupList = inp()
+# print("Questions with answer 'yes': ", groupList)
+listOfTotalYes = calc_num_of_yes(groupList)
+# print("No of questions with Yes answer for all groups: ", listOfTotalYes)
+totalYes = calc_total_yes(listOfTotalYes)
+print("Total yes: ", totalYes)
